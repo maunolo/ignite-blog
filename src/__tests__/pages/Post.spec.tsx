626c4d9bd144221a3888f5b5cab730fca9828af7
Mauricio Nogueira Lopes <mauuricionlopes@gmail.com>
2021-03-25 11:16:46 -0300

    fixing post spec

--- src/__tests__/pages/Post.spec.tsx
+++ src/__tests__/pages/Post.spec.tsx
@@ -8,7 +8,7 @@ import { ParsedUrlQuery, parse } from 'querystring';
 
 import { useRouter } from 'next/router';
 import { getPrismicClient } from '../../services/prismic';
-import Post, { getStaticProps, getStaticPaths } from '../../pages/posts/[slug]';
+import Post, { getStaticProps, getStaticPaths } from '../../pages/post/[slug]';
 
 interface Post {
   first_publication_date: string | null;
