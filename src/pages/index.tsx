62c597ae7e5dac3806a3e06ec475eac18af1ff2b
Mauricio Nogueira Lopes <mauuricionlopes@gmail.com>
2021-03-25 10:35:06 -0300

    fixing home lint and css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,52 @@
 import { GetStaticProps } from 'next';
 
-import { getPrismicClient } from '../services/prismic'
-import Prismic from '@prismicio/client'
+import Prismic from '@prismicio/client';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 import styles from './home.module.scss';
-
-import { FiCalendar, FiUser } from 'react-icons/fi'
-
-import { format } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
-import { useState } from 'react'
-import { Loader } from '../components/Loader'
+import { getPrismicClient } from '../services/prismic';
+import { Loader } from '../components/Loader';
 
 interface Post {
-  uid?: string
-  first_publication_date: string | null
+  uid?: string;
+  first_publication_date: string | null;
   data: {
-    title: string
-    subtitle: string
-    author: string
-  }
+    title: string;
+    subtitle: string;
+    author: string;
+  };
 }
 
 interface PostPagination {
-  next_page: string | null
-  results: Post[]
+  next_page: string | null;
+  results: Post[];
 }
 
 interface HomeProps {
-  postsPagination: PostPagination
+  postsPagination: PostPagination;
 }
 
-export default function Home({ postsPagination }: HomeProps) {
-  const [posts, setPosts] = useState<Post[]>(postsPagination.results)
-  const [next_page, setNextPage] = useState<string>(postsPagination.next_page)
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
+  const [next_page, setNextPage] = useState<string>(postsPagination.next_page);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  async function loadMorePosts() {
-    setIsLoading(true)
+  async function loadMorePosts(): Promise<void> {
+    setIsLoading(true);
     setTimeout(() => {
-      setIsLoading(false)
-    }, 5000)
+      setIsLoading(false);
+    }, 5000);
     fetch(next_page)
       .then(response => response.json())
       .then(data => {
-        setPosts([...posts, ...data.results])
-        setNextPage(data.next_page)
-        setIsLoading(false)
-      })
+        setPosts([...posts, ...data.results]);
+        setNextPage(data.next_page);
+        setIsLoading(false);
+      });
   }
 
   return (
@@ -56,50 +55,49 @@ export default function Home({ postsPagination }: HomeProps) {
         <img src="/logo.svg" alt="spacetraveling" />
       </header>
       <main className={styles.posts}>
-        { posts?.map(post => (
+        {posts?.map(post => (
           <a key={post.uid} href={`/posts/${post.uid}`}>
             <h1>{post.data.title}</h1>
             <p>{post.data.subtitle}</p>
-            <div className={styles.postDetails}>
+            <div className={styles.postInfo}>
               <FiCalendar />
-              <time>{
-                format(
-                  new Date(post.first_publication_date),
-                  "dd MMM yyyy",
-                  { locale: ptBR }
-                )
-              }</time>
+              <time>
+                {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
+                  locale: ptBR,
+                })}
+              </time>
               <FiUser />
               <span>{post.data.author}</span>
             </div>
           </a>
-        )) }
+        ))}
       </main>
-      { next_page && (
+      {next_page && (
         <div className={styles.loadMorePosts}>
-          <a href="#" onClick={loadMorePosts}>
+          <button type="button" onClick={loadMorePosts}>
             Carregar mais posts
-          </a>
+          </button>
           <Loader isLoading={isLoading} />
         </div>
       )}
     </div>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient()
-  const postsResponse = await prismic.query([
-    Prismic.predicates.at('document.type', 'post')
-  ], {
-    fetch: ['post.title', 'post.subtitle', 'post.author'],
-    pageSize: 20,
-  })
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.subtitle', 'post.author'],
+      pageSize: 20,
+    }
+  );
 
   return {
     props: {
-      postsPagination: postsResponse
+      postsPagination: postsResponse,
     },
     revalidate: 60 * 60 * 12, // 12 hours
-  }
-}
+  };
+};
